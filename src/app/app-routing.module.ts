b6f79c65de8f0308ee13a7321bb8f1b7ac592570
Romain <romain@nutrinomics.care>
2023-03-08 13:18:27 +0100

    create welcome page and routing + main nav component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  {
+  path: '',
+  redirectTo: 'welcome',
+  pathMatch: 'full'
+  },
+  {
+    path: 'welcome',
+    component: WelcomeComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { MainNavComponent } from './shared/components/main-nav/main-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
+    MainNavComponent,
   ],
   imports: [
     BrowserModule,
